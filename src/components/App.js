2b625ae86bf17bac2a1c2c33a6bf3ed8fd4ed178
Armen Merikyan <motoversemain@gmail.com>
2023-09-16 17:16:22 -0700

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { HashRouter, Route, Switch } from "react-router-dom";
+import { HashRouter, Route, Switch, Redirect } from "react-router-dom";
 import "./App.css";
 import Web3 from "web3";
 
